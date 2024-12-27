7d4cd7b8153f8c0a3f7923ca92fb2c79e2ce7f01
Gaurav-as-u-know <gauravkhunti@gmail.com>
2024-12-27 21:46:48 +0530

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ranjitray/ros_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ranjitray/ws/devel;/opt/ros/noetic'.split(';') if '/home/ranjitray/ws/devel;/opt/ros/noetic' != '' else []
